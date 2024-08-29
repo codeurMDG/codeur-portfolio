4f35e247bf4ccad1510e4eb6bb59269eea8f3bd4
CodingMDG <codeurmdg@gmail.com>
2024-08-29 22:32:40 +0300

    Feat: add Nav menu for mobile version

--- components/layout/Nav.jsx
+++ components/layout/Nav.jsx
@@ -2,29 +2,7 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-
-const links = [
-    {
-        name: "home",
-        path: "/"
-    },
-    {
-        name: "services",
-        path: "/services"
-    },
-    {
-        name: "resume",
-        path: "/resume"
-    },
-    {
-        name: "work",
-        path: "/work"
-    },
-    {
-        name: "contact",
-        path: "/contact"
-    },
-]
+import links from "../path/links"
 
 const Nav = () => {
     const pathName = usePathname()
