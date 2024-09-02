0a5579d7848e87403fcb510cab6a638763184a10
CodingMDG <codeurmdg@gmail.com>
2024-09-02 21:32:52 +0300

    Feat: add experience components in resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -8,6 +8,7 @@ import { delay, motion } from "framer-motion"
 // Components 
 import about from "@/components/path/about"
 import skills from "@/components/path/skills"
+import experiences from "@/components/path/experiences"
 
 
 const Resume = () => {
@@ -25,19 +26,50 @@ const Resume = () => {
 		className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
 		>
 			<div className="container mx-auto">
-				<Tabs defaultValue="skills" className="flex flex-col xl:flex-row gap-[60px]">
+				<Tabs defaultValue="experiences" className="flex flex-col xl:flex-row gap-[60px]">
 					<TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+						<TabsTrigger value="experiences">Experience</TabsTrigger>
 						<TabsTrigger value="skills">Skills</TabsTrigger>
 						<TabsTrigger value="about">About me</TabsTrigger>
 					</TabsList>
 
 					{/* Content */}
-					<div className="min-h-[70vh w-full]">
+					<div className="min-h-[70vh] w-full">
+						{/* Experience */}
+						<TabsContent value="experiences" className="w-full">
+							<div className="flex flex-col gap-[30px] text-center xl:text-left">
+								<h3 className="text-4xl font-bold">{ experiences.title }</h3>
+								<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{ experiences.description }</p>
+								<ScrollArea className="h-[400px]">
+									<ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+										{experiences.items.map((item, index)=> {
+											return (
+												<li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col items-center justify-center lg:items-start gap-1">
+													<span className="text-accent">{item.duration}</span>
+													<h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+													<div className="flex items-center gap-3">
+														{/* dot */}
+														<span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+														<p className="text-white/60">{item.company}</p>
+													</div>
+												</li>
+											)
+										})}
+									</ul>
+								</ScrollArea>
+							</div>
+						</TabsContent>
+
 						{/* Skills */}
-						<TabsContent value="skills" className="w-full">Skills</TabsContent>
+						<TabsContent value="skills" className="w-full">
+							<div className="flex flex-col gap-[30px] text-center xl:text-left">
+								<h3 className="text-4xl font-bold">{ skills.title }</h3>
+								<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{ skills.description }</p>
+							</div>
+						</TabsContent>
 
 						{/* About */}
-						{/* <TabsContent value="about" className="about">About</TabsContent> */}
+						<TabsContent value="about" className="w-full">About</TabsContent>
 					</div>
 				</Tabs>
 			</div>
