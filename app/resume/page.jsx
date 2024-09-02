c5d60d8f40e476e9ca9b51f3e4db52a82bf4f3da
CodingMDG <codeurmdg@gmail.com>
2024-09-02 19:02:33 +0300

    Feat: add information of resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,8 @@
+"use client"
+
+import { FaHtml5, FaCss3, FaJs, FaPhp, FaReact } from "react-icons/fa"
+import { SiSymfony, SiSass, SiWordpress, SiWebpack, SiNextdotjs, SiTailwindcss, SiBootstrap, SiElementor, SiMarkdown, SiMysql } from "react-icons/si"
+
 const Resume = () => {
   return (
     <div>Resume page</div>
---
+++ components/path/experience.jsx
@@ -0,0 +1,31 @@
+
+const experience = {
+    icon: "",
+    title: "My experience",
+    description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Eleifend libero dictum faucibus odio magna. Dictumst at lobortis, pellentesque nisi sem ante blandit maecenas nulla. Rutrum dictumst hac morbi, eros laoreet in suscipit aliquam. Integer tortor quam nibh sem mus porttitor laoreet. Orci dictum ut inceptos tortor eu ligula ornare. Praesent mi porta natoque, dolor leo nisi scelerisque netus. Sagittis leo nisi arcu interdum potenti semper senectus odio. Aptent placerat quis maecenas curabitur senectus vehicula? Curae vivamus vulputate habitasse cubilia vitae erat.",
+    items: [
+        {
+            company: "Freelance",
+            position: "Front-end Developer",
+            duration: "Mai 2024 - Present"
+        },
+        {
+            company: "Freelance",
+            position: "WordPress Developer",
+            duration: "Janvier - Mai 2024"
+        }, 
+        {
+            company: "Nir'info",
+            position: "Web Developer",
+            duration: "2023 - 2024"
+        },  
+
+        {
+            company: "Nir'info",
+            position: "Formateur",
+            duration: "2021 - 2023"
+        }, 
+    ]
+}
+
+export default experience
