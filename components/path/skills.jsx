3b1bf13e2a683a1a42bf822a114c30c58a698da7
CodingMDG <codeurmdg@gmail.com>
2024-09-02 21:54:30 +0300

    Feat: add skills component in resume page

--- components/path/skills.jsx
+++ components/path/skills.jsx
@@ -3,7 +3,7 @@ import { SiSass, SiWordpress, SiWebpack, SiNextdotjs, SiTailwindcss, SiBootstrap
 
 const skills =  {
     title: "My skills", 
-    description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Eleifend libero dictum faucibus odio magna. Dictumst at lobortis, pellentesque nisi sem ante blandit maecenas nulla. Rutrum dictumst hac morbi, eros laoreet in suscipit aliquam. Integer tortor quam nibh sem mus porttitor laoreet. Orci dictum ut inceptos tortor eu ligula ornare. Praesent mi porta natoque, dolor leo nisi scelerisque netus. Sagittis leo nisi arcu interdum potenti semper senectus odio. Aptent placerat quis maecenas curabitur senectus vehicula? Curae vivamus vulputate habitasse cubilia vitae erat.",
+    description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Eleifend libero dictum faucibus odio magna.",
     skillList: [
         {
             icon: <FaHtml5/>,
