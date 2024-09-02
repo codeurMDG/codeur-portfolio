c54c49fadbfb22d1df6b35b0baed3d59634d359c
CodingMDG <codeurmdg@gmail.com>
2024-09-02 22:06:10 +0300

    Feat: add about component in resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -89,7 +89,22 @@ const Resume = () => {
 						</TabsContent>
 
 						{/* About */}
-						<TabsContent value="about" className="w-full">About</TabsContent>
+						<TabsContent value="about" className="w-full text-center xl:text-left">
+							<div className="flex flex-col gap-[30px]">
+								<h3 className="text-4xl font-bold">{ about.title }</h3>
+								<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{ about.description }</p>
+								<ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+									{about.info.map((item, index) => {
+										return (
+											<li key={index} className="flex items-center justify-center gap-4 xl:justify-start">
+												<span className="text-white/60">{item.fieldName}</span>
+												<span className="text-xl">{item.fieldValue}</span>
+											</li>
+										)
+									})}
+								</ul>
+							</div>
+						</TabsContent>
 					</div>
 				</Tabs>
 			</div>
--- components/path/about.jsx
+++ components/path/about.jsx
@@ -1,7 +1,7 @@
 
 const about = {
     title: "About me",
-    description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Eleifend libero dictum faucibus odio magna. Dictumst at lobortis, pellentesque nisi sem ante blandit maecenas nulla. Rutrum dictumst hac morbi, eros laoreet in suscipit aliquam. Integer tortor quam nibh sem mus porttitor laoreet. Orci dictum ut inceptos tortor eu ligula ornare. Praesent mi porta natoque, dolor leo nisi scelerisque netus. Sagittis leo nisi arcu interdum potenti semper senectus odio. Aptent placerat quis maecenas curabitur senectus vehicula? Curae vivamus vulputate habitasse cubilia vitae erat.",
+    description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Eleifend libero dictum faucibus odio magna. ",
     info: [
         {
             fieldName: "Name",
