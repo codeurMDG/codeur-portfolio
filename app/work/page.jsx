4352c50bb11c0e4f22be8404ca90059d8c61c3b5
CodingMDG <codeurmdg@gmail.com>
2024-09-03 17:01:10 +0300

    Feat: add information of work components in work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -10,10 +10,74 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/comp
 import Link from "next/link"
 import Image from "next/image"
 
+// components : 
+import projects from "@/components/path/projects"
 
 const Work = () => {
+	const [project, setProject] = useState(projects[0])
 	return (
-		<div>Work page</div>
+		<motion.section 
+			initial={{opacity: 0}}
+			animate={{opacity: 1}}
+			className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+		>
+			<div className="container mx-auto">
+				<div className="flex flex-col xl:flex-row xl:gap-[30px]">
+					<div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+						<div className="flex flex-col gap-[30px] h-[50%]">
+							{/* Outline number */}
+							<div className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.num}</div>
+							{/* project category */}
+							<h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category} project</h2>
+							{/* project description */}
+							<p className="text-white/60">{project.description}</p>
+							{/* project stack */}
+							<ul className="flex gap-3 flex-wrap">
+								{project.stack.map((item, index) => {
+									return <li key={index} className="text-[18px] text-accent">
+										{item.name}
+										{/* Remove the last comma */}
+										{index !== project.stack.length - 1 && ","}
+									</li>
+								} )}
+							</ul>
+							{/* Border */}
+							<div className="border border-white/20"></div>
+							{/* Buttons */}
+							<div className="flex items-center gap-4">
+								{/* Live project button */}
+								<Link href={project.live}>
+									<TooltipProvider delayDuration={100}>
+										<Tooltip>
+											<TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+												<BsArrowUpRight className="text-white text-3xl group-hover:text-accent"/>
+											</TooltipTrigger>
+											<TooltipContent>
+												<p>live project</p>
+											</TooltipContent>
+										</Tooltip>
+									</TooltipProvider>
+								</Link>
+								{/* GitHub project button */}
+								<Link href={project.github}>
+									<TooltipProvider delayDuration={100}>
+										<Tooltip>
+											<TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+												<BsGithub className="text-white text-3xl group-hover:text-accent"/>
+											</TooltipTrigger>
+											<TooltipContent>
+												<p>GitHub repository</p>
+											</TooltipContent>
+										</Tooltip>
+									</TooltipProvider>
+								</Link>
+							</div>
+						</div>
+					</div>
+					<div className="w-full xl:w-[50%]">Slider</div>
+				</div>
+			</div>
+		</motion.section>
 	)
 }
 
--- components/path/projects.jsx
+++ components/path/projects.jsx
@@ -5,7 +5,7 @@ const projects = [
         category: "Fullstack",
         title: "Igoblog",
         description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
-        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name: "Javascript", name: "PHP", name: "WordPress"}],
+        stack: [{name: "Html 5"}, {name: "Css 3"}, {name:"Bootstrap"}, {name: "Javascript"}, {name: "PHP"}, {name: "Wordpress"}],
         image: "/assets/works/Igoblog.png",
         live: "https://igoblog.fr/",
         github: "https://github.com/RovaFitia"
@@ -15,7 +15,7 @@ const projects = [
         category: "Fullstack",
         title: "Nir'info",
         description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
-        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name: "Javascript", name: "PHP", name: "WordPress"}],
+        stack: [{name: "Html 5"}, {name: "Css 3"}, {name:"Bootstrap"}, {name: "Javascript"}, {name: "PHP"}, {name: "Wordpress"}],
         image: "/assets/works/Nir-info.png",
         live: "https://nir-info.mg/",
         github: "https://github.com/RovaFitia"
@@ -25,7 +25,7 @@ const projects = [
         category: "Fullstack",
         title: "Leslandines",
         description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
-        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name: "Javascript", name: "PHP", name: "Wix"}],
+        stack: [{name: "Html 5"}, {name: "Css 3"}, {name:"Bootstrap"}, {name: "Javascript"}, {name: "PHP"}, {name: "Wix"}],
         image: "/assets/works/Leslandines.png",
         live: "https://www.leslandines.com/",
         github: "https://github.com/RovaFitia"
@@ -35,7 +35,7 @@ const projects = [
         category: "WordPress",
         title: "Myexpat",
         description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
-        stack: [{name: "Wordpress", name: "Divi builder"}],
+        stack: [{name: "Wordpress"}, {name: "Divi builder"}],
         image: "/assets/works/myexpat.png",
         live: "https://www.myexpat.fr/",
         github: "https://github.com/RovaFitia"
@@ -45,7 +45,7 @@ const projects = [
         category: "Frontend",
         title: "Espace client Investissement Locatif",
         description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
-        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name:"Scss", name: "Javascript", name: "Webpack"}],
+        stack: [{name: "Html 5"}, {name: "Css 3"}, {name:"Bootstrap"}, {name:"Scss"}, {name: "Javascript"}, {name: "Webpack"}],
         image: "/assets/works/Investissement-locatif.png",
         live: "https://investissement-locatif.com/",
         github: "https://github.com/RovaFitia"
@@ -55,7 +55,7 @@ const projects = [
         category: "Frontend",
         title: "Espace admin Investissement Locatif",
         description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
-        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name:"Scss", name: "Javascript", name: "Webpack"}],
+        stack: [{name: "Html 5"}, {name: "Css 3"}, {name:"Bootstrap"}, {name:"Scss"}, {name: "Javascript"}, {name: "Webpack"}],
         image: "/assets/works/IL-dashboard.png",
         live: "https://investissement-locatif.com/",
         github: "https://github.com/RovaFitia"
@@ -65,7 +65,7 @@ const projects = [
         category: "Frontend",
         title: "Nousgerons",
         description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
-        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name:"Scss", name: "Javascript", name: "Webpack"}],
+        stack: [{name: "Html 5"}, {name: "Css 3"}, {name:"Bootstrap"}, {name:"Scss"}, {name: "Javascript"}, {name: "Webpack"}],
         image: "/assets/works/nousgerons.png",
         live: "https://nousgerons.com/",
         github: "https://github.com/RovaFitia"
@@ -75,7 +75,7 @@ const projects = [
         category: "Frontend",
         title: "Refonte Investissement Locatif",
         description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
-        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name:"Scss", name: "Javascript", name: "Webpack"}],
+        stack: [{name: "Html 5"}, {name: "Css 3"}, {name:"Bootstrap"}, {name:"Scss"}, {name: "Javascript"}, {name: "Webpack"}],
         image: "/assets/works/IL.png",
         live: "https://investissement-locatif.com/",
         github: "https://github.com/RovaFitia"
@@ -85,7 +85,7 @@ const projects = [
         category: "Wordpress",
         title: "Beetravel",
         description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
-        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name:"Scss", name: "Javascript", name: "Webpack"}],
+        stack: [{name: "Wordpress"}, {name: "Elementor"}],
         image: "/assets/works/beetravel.png",
         live: "https://github.com/RovaFitia",
         github: "https://github.com/RovaFitia"
