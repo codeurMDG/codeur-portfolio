27aea204c91578bc179e4890171ca2bbb6fd7351
CodingMDG <codeurmdg@gmail.com>
2024-09-03 11:49:57 +0300

    Feat: add data of works components

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,20 @@
+"use client"
+
+import { motion } from "framer-motion"
+import React, { useState } from "react"
+import {Swiper, SwiperSlide} from "swiper/react"
+import "swiper/css"
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+import Link from "next/link"
+import Image from "next/image"
+
+
 const Work = () => {
-  return (
-    <div>Work page</div>
-  )
+	return (
+		<div>Work page</div>
+	)
 }
 
 export default Work
---
+++ components/path/projects.jsx
@@ -0,0 +1,95 @@
+
+const projects = [
+    {
+        num: "01",
+        category: "Fullstack",
+        title: "Igoblog",
+        description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
+        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name: "Javascript", name: "PHP", name: "WordPress"}],
+        image: "/assets/works/Igoblog.png",
+        live: "https://igoblog.fr/",
+        github: "https://github.com/RovaFitia"
+    },
+    {
+        num: "02",
+        category: "Fullstack",
+        title: "Nir'info",
+        description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
+        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name: "Javascript", name: "PHP", name: "WordPress"}],
+        image: "/assets/works/Nir-info.png",
+        live: "https://nir-info.mg/",
+        github: "https://github.com/RovaFitia"
+    },
+    {
+        num: "03",
+        category: "Fullstack",
+        title: "Leslandines",
+        description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
+        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name: "Javascript", name: "PHP", name: "Wix"}],
+        image: "/assets/works/Leslandines.png",
+        live: "https://www.leslandines.com/",
+        github: "https://github.com/RovaFitia"
+    },
+    {
+        num: "04",
+        category: "WordPress",
+        title: "Myexpat",
+        description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
+        stack: [{name: "Wordpress", name: "Divi builder"}],
+        image: "/assets/works/myexpat.png",
+        live: "https://www.myexpat.fr/",
+        github: "https://github.com/RovaFitia"
+    },
+    {
+        num: "05",
+        category: "Frontend",
+        title: "Espace client Investissement Locatif",
+        description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
+        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name:"Scss", name: "Javascript", name: "Webpack"}],
+        image: "/assets/works/Investissement-locatif.png",
+        live: "https://investissement-locatif.com/",
+        github: "https://github.com/RovaFitia"
+    },
+    {
+        num: "06",
+        category: "Frontend",
+        title: "Espace admin Investissement Locatif",
+        description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
+        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name:"Scss", name: "Javascript", name: "Webpack"}],
+        image: "/assets/works/IL-dashboard.png",
+        live: "https://investissement-locatif.com/",
+        github: "https://github.com/RovaFitia"
+    },
+    {
+        num: "07",
+        category: "Frontend",
+        title: "Nousgerons",
+        description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
+        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name:"Scss", name: "Javascript", name: "Webpack"}],
+        image: "/assets/works/nousgerons.png",
+        live: "https://nousgerons.com/",
+        github: "https://github.com/RovaFitia"
+    },
+    {
+        num: "08",
+        category: "Frontend",
+        title: "Refonte Investissement Locatif",
+        description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
+        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name:"Scss", name: "Javascript", name: "Webpack"}],
+        image: "/assets/works/IL.png",
+        live: "https://investissement-locatif.com/",
+        github: "https://github.com/RovaFitia"
+    },
+    {
+        num: "09",
+        category: "Wordpress",
+        title: "Beetravel",
+        description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl urna tempor dictumst orci consequat senectus inceptos rhoncus curae. Mus tristique leo conubia suspendisse, litora praesent. Nec parturient metus fermentum adipiscing mattis dictum vitae pulvinar.",
+        stack: [{name: "Html 5", name: "Css 3", name:"Bootstrap", name:"Scss", name: "Javascript", name: "Webpack"}],
+        image: "/assets/works/beetravel.png",
+        live: "https://github.com/RovaFitia",
+        github: "https://github.com/RovaFitia"
+    },
+]
+
+export default projects
