f1ff772a7be8f9c5fa5190334e9c61ba78a41996
CodingMDG <codeurmdg@gmail.com>
2024-09-04 19:10:40 +0300

    Feat: add info components in contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
 			<div className="container mx-auto">
 				<div className="flex flex-col xl:flex-row gap-[30px]">
 					{/* form */}
-					<div className="xl:h-[54%] order-2 xl:order-none">
+					<div className="xl:w-[54%] order-2 xl:order-none">
 						<form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
 							<h3 className="text-4xl text-accent">Let&apos;s work together</h3>
 							<p className="text-white/60">Lorem ipsum odor amet, consectetuer adipiscing elit. Mus phasellus aptent commodo vehicula blandit torquent semper ullamcorper. Egestas mus at rhoncus aptent sagittis tincidunt amet.</p>
@@ -48,7 +48,7 @@ const Contact = () => {
 									<SelectGroup>
 										<SelectLabel>Select a service</SelectLabel>
 										<SelectItem value="front">Frontend </SelectItem>
-										<SelectItem value="wordpress">Wordpress development</SelectItem>
+										<SelectItem value="wordpress">Wordpress</SelectItem>
 										<SelectItem value="seo">SEO</SelectItem>
 									</SelectGroup>
 								</SelectContent>
@@ -61,7 +61,19 @@ const Contact = () => {
 					</div>
 					{/* info */}
 					<div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-						info
+						<ul className="flex flex-col gap-10">
+							{info.map((item, index) => {
+								return <li key={index} className="flex items-center gap-6">
+									<div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+										<div className="text-[28px]">{item.icon}</div>
+									</div>
+									<div className="flex-1">
+										<p className="text-white/60">{item.title}</p>
+										<h3 className="text-xl">{item.description}</h3>
+									</div>
+								</li>
+							})}
+						</ul>
 					</div>
 				</div>
 			</div>
