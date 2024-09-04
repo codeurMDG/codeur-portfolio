7d2828022a63955319e92bcee7b7c65e1c0ae62b
CodingMDG <codeurmdg@gmail.com>
2024-09-04 10:44:37 +0300

    Feat: add slider image for work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -12,6 +12,7 @@ import Image from "next/image"
 
 // components : 
 import projects from "@/components/path/projects"
+import WorkSliderBtns from "@/components/WorkSliderBtns"
 
 const Work = () => {
 	const [project, setProject] = useState(projects[0])
@@ -27,12 +28,12 @@ const Work = () => {
 	return (
 		<motion.section 
 			initial={{opacity: 0}}
-			animate={{opacity: 1}}
+			animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"}}}
 			className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
 		>
 			<div className="container mx-auto">
 				<div className="flex flex-col xl:flex-row xl:gap-[30px]">
-					<div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+					<div className="w-full xl:w-[40%] xl:h-[480px] flex flex-col xl:justify-between order-2 xl:order-none">
 						<div className="flex flex-col gap-[30px] h-[50%]">
 							{/* Outline number */}
 							<div className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.num}</div>
@@ -83,18 +84,18 @@ const Work = () => {
 							</div>
 						</div>
 					</div>
-					<div className="w-full xl:w-[50%]">
+					<div className="w-full xl:w-[60%]">
 						<Swiper 
 							spaceBetween={30} 
 							slidesPerView={1} 
-							className="xl:h-[520px] mb-12"
+							className="xl:h-[550px] mb-12"
 							onSlideChange={handleSlideChange}
 						>
 							{projects.map((project, index) => {
 								return <SwiperSlide key={index} className="w-full">
-									<div className="h-[460px] realative group flex justify-center items-center bg-pink-50/20">
+									<div className="h-[475px] realative group flex justify-center items-center bg-pink-50/20">
 										{/* overlay */}
-										<div></div>
+										<div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
 										{/* image */}
 										<div className="w-full h-full relative">
 											<Image src={project.image} fill className="object-cover" alt="work"/>
@@ -102,6 +103,12 @@ const Work = () => {
 									</div>
 								</SwiperSlide>
 							})}
+
+							{/* slider buttons */}
+							<WorkSliderBtns 
+								containerStyle="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+								btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] rounded-sm flex justify-center items-center transition-all duration-500"
+							/>
 						</Swiper>
 					</div>
 				</div>
