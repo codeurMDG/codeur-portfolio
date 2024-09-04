1232e40f6873a77f5cb1d390051a585168306fcf
CodingMDG <codeurmdg@gmail.com>
2024-09-04 16:26:00 +0300

    Feat: add form, input, select, textarea and btn components in contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Select, SelectGroup, SelectContent, SelectLabel, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { motion } from "framer-motion"
+
+import info from "@/components/path/info"
+
+
+
 
 const Contact = () => {
-  return (
-    <div>Contact page</div>
-  )
+	return (
+		<motion.section
+			initial={{opacity: 0}}
+			animate={{
+				opacity: 1,
+				transition: {
+					delay: 2.4,
+					duration: 0.4,
+					ease: "easeIn"
+				}
+			}}
+			className="py-6"
+		>
+			<div className="container mx-auto">
+				<div className="flex flex-col xl:flex-row gap-[30px]">
+					{/* form */}
+					<div className="xl:h-[54%] order-2 xl:order-none">
+						<form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+							<h3 className="text-4xl text-accent">Let&apos;s work together</h3>
+							<p className="text-white/60">Lorem ipsum odor amet, consectetuer adipiscing elit. Mus phasellus aptent commodo vehicula blandit torquent semper ullamcorper. Egestas mus at rhoncus aptent sagittis tincidunt amet.</p>
+							{/* input */}
+							<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+								<Input type="firstname" placeholder="Firstname"/>
+								<Input type="lastname" placeholder="Lastname"/>
+								<Input type="email" placeholder="Email address"/>
+								<Input type="phone" placeholder="Phone number"/>
+							</div>
+							{/* select */}
+							<Select>
+								<SelectTrigger className="w-full">
+									<SelectValue placeholder="Select as service"/>
+								</SelectTrigger>
+								<SelectContent>
+									<SelectGroup>
+										<SelectLabel>Select a service</SelectLabel>
+										<SelectItem value="front">Frontend </SelectItem>
+										<SelectItem value="wordpress">Wordpress development</SelectItem>
+										<SelectItem value="seo">SEO</SelectItem>
+									</SelectGroup>
+								</SelectContent>
+							</Select>
+							{/* Textarea */}
+							<Textarea className="h-[200px]" placeholder="Type your message here." />
+							{/* Button */}
+							<Button size="md" className="max-w-40">Send message</Button>
+						</form>
+					</div>
+					{/* info */}
+					<div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+						info
+					</div>
+				</div>
+			</div>
+		</motion.section>
+	)
 }
 
 export default Contact
