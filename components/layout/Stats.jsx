7bca52f6efa3c94dc0cc0ba1023571d35601676a
CodingMDG <codeurmdg@gmail.com>
2024-09-07 07:44:12 +0300

    Fix: edit stats components

--- components/layout/Stats.jsx
+++ components/layout/Stats.jsx
@@ -11,7 +11,7 @@ const Stats = () => {
             <div className="container mx-auto">
                 <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
                     {stats.map((item, index) => {
-                        return <div key={index} className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
+                        return <div key={index} className="flex-1 flex gap-4 items-center justify-start xl:justify-start">
                             <CountUp end={item.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold" />
                             <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>{item.text}</p>
                         </div>
